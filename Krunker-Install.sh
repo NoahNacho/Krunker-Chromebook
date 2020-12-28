556c4f1b0da36ddbaf2f45a8685061f3de357635
Noah <63881698+NoahNacho@users.noreply.github.com>
2020-12-27 22:22:17 -0500

    Update Krunker-Install.sh

--- Krunker-Install.sh
+++ Krunker-Install.sh
@@ -4,4 +4,5 @@ sudo apt install libnss3
 #Krunker install
 wget https://client2.krunker.io/setup.AppImage
 mv setup.AppImage Krunker.AppImage
+chmod +x Krunker.AppImage
 echo 'run "./Krunker.AppImage" in the terminal anytime to start krunker.'
