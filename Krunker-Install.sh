b05c9d06ca1604547669b0cafad6bde260e4a1e9
Noah <63881698+NoahNacho@users.noreply.github.com>
2020-12-28 15:38:00 -0500

    Update Krunker-Install.sh
    added desktop shortcut creation and desktop icon.

--- Krunker-Install.sh
+++ Krunker-Install.sh
@@ -7,11 +7,12 @@ wget https://client2.krunker.io/setup.AppImage
 mv setup.AppImage Krunker.AppImage
 chmod +x Krunker.AppImage
 touch /home/$who/Krunker.desktop
+wget https://raw.githubusercontent.com/NoahNacho/Krunker-Chromebook/main/Krunk.png
 echo "Type=Application" >> Krunker.desktop
 echo "Terminal=false" >> Krunker.desktop
 echo "Name=Krunker" >> Krunker.desktop
-echo "Icon=/home/noah/Downloads/profilemask.jpg" >> Krunker.desktop
-echo "Exec=/home/noah/Desktop/setup.AppImage" >> Krunker.desktop
+echo "Icon=/home/$who/Krunk.png" >> Krunker.desktop
+echo "Exec=/home/$who/Krunker.AppImage" >> Krunker.desktop
 echo "StartupNotify=true" >> Krunker.desktop
 chmod +x /home/$who/Krunker.desktop
 echo 'There should be a Desktop Icon to run Krunker from. if not you can go to a Terminal anytime and run "./Krunker.AppImage"'
